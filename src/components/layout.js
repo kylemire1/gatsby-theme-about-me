f74e2335101adf348704f7d1b450b0aa68a57c94
Kyle Lemire <kyle@elykinnovation.com>
2020-01-05 17:52:03 -0600

    feat: added conditional logic to hide contact link if no link is set in options

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,19 +13,19 @@ const Layout = ({ children }) => {
     }
   `);
 
-  console.log(data);
-
   return (
     <ThemeLayout>
       <Header>
         <span>Gatsby Theme About Me</span>
-        <nav>
-          <Styled.ul>
-            <li>
-              <Link to={data.site.siteMetadata.contactLink}>Contact</Link>
-            </li>
-          </Styled.ul>
-        </nav>
+        {data.site.siteMetadata.contactLink.length > 0 && (
+          <nav>
+            <Styled.ul>
+              <li>
+                <Link to={data.site.siteMetadata.contactLink}>Contact</Link>
+              </li>
+            </Styled.ul>
+          </nav>
+        )}
       </Header>
       <Main>{children}</Main>
     </ThemeLayout>
